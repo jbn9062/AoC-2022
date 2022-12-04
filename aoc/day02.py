dfb2becfb8d6b2376289249ab49acc275b834739
Jibin varghese <jbnjibin906@gmail.com>
2022-12-04 13:21:50 +0100

    AoC

--- aoc/day02.py
+++ aoc/day02.py
@@ -1,42 +1,33 @@
 
 
 def getInput():
-    with open("../../../AppData/Roaming/JetBrains/PyCharmCE2022.2/scratches/day02.txt", "r") as f:
+    with open("day02.txt", "r") as f:
         x = [list(i) for i in f.read().split('\n')]
     for i in x:
         i.remove(' ')
     return x
 
 def findScore(x):
-    points = {'A': 1, 'B': 2, 'C': 3}
+    points = {'A': 1, 'B': 2, 'C': 3, 'X': 1, 'Y': 2, 'Z': 3}
     score = 0
     for i in x:
         for j in i:
-            if j == 'A':
-                if i[0] == 'X':
-                    score = score + (points.get('A') * 2)
-                elif j+1 == 'Y':
-                    score = score + points.get('A')
-                elif j+1 == 'Z':
-                    score = score + points.get('A') + 6
-            while j == 'B':
-                if j+1 == 'X':
-                    score = score + points.get('A') + 6
-                elif j+1 == 'Y':
-                    score = score + points.get('A') * 2
-                elif i+1 == 'Z':
-                    score = score + points.get('A')
-            while i == 'C':
-                if i+1 == 'X':
-                    score = score + points.get('A')
-                elif i+1 == 'Y':
-                    score = score + points.get('A') + 6
-                elif i+1 == 'Z':
-                    score = score + (points.get('A')) * 2
+            if j[0] == 'A':
+                print(j[-1])
+                score = points['A'] + points['X']
+                print(score)
+            elif j[0] == 'A' and j[-1] == 'Y':
+                score = score + points.get('A')
+            elif j[0] == 'A' and j[-1] == 'Z':
+                score = score + points.get('A') + 6
     print(points)
     return score
 
-
-
+x = getInput()
 print(getInput())
-print(findScore(getInput()))
+print(findScore(x))
+
+#********************TEST********************
+'''def test_findScore():
+    y = [('A', 'Y'), ('B', 'X'), ('C', 'Z')]
+    assert findScore(y) == 15'''
---
+++ aoc/test_day02.py
@@ -0,0 +1,4 @@
+#import day02
+
+y = [('A', 'Y'), ('B', 'X'), ('C', 'Z')]
+assert findScore(y) == 15
