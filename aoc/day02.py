0b4a929eca776795efc0b68432895630aa266403
Jibin varghese <jbnjibin906@gmail.com>
2022-12-05 17:19:53 +0100

    AoC

--- aoc/day02.py
+++ aoc/day02.py
@@ -1,5 +1,4 @@
 
-
 def getInput():
     with open("day02.txt", "r") as f:
         x = [list(i) for i in f.read().split('\n')]
@@ -10,24 +9,56 @@ def getInput():
 def findScore(x):
     points = {'A': 1, 'B': 2, 'C': 3, 'X': 1, 'Y': 2, 'Z': 3}
     score = 0
-    for i in x:
-        for j in i:
-            if j[0] == 'A':
-                print(j[-1])
-                score = points['A'] + points['X']
-                print(score)
-            elif j[0] == 'A' and j[-1] == 'Y':
-                score = score + points.get('A')
-            elif j[0] == 'A' and j[-1] == 'Z':
-                score = score + points.get('A') + 6
+    for i, j in x:
+        if i == 'A' and j == 'X':
+            score = score + points.get('X') + 3
+        elif i == 'A' and j == 'Y':
+            score = score + points.get('Y') + 6
+        elif i == 'A' and j == 'Z':
+            score = score + points.get('Z')
+        elif i == 'B' and j =='X':
+            score = score + points.get('X')
+        elif i == 'B' and j == 'Y':
+            score = score + points.get('Y') + 3
+        elif i == 'B' and j == 'Z':
+            score = score + points.get('Z') + 6
+        elif i == 'C' and j == 'X':
+            score = score + points.get('X') + 6
+        elif i == 'C' and j == 'Y':
+            score = score + points.get('Y')
+        elif i == 'C' and j == 'Z':
+            score = score + points.get('Z') + 3
+
     print(points)
     return score
+
+def task2(x):
+    points = {'A': 1, 'B': 2, 'C': 3, 'X': 1, 'Y': 2, 'Z': 3}
+    score2 = 0
+    for i in range(len(x)):
+
+    for i, j in x:
+        if i == 'A' and j == 'X':
+            score = score2 + points.get('X') + 3
+        elif i == 'A' and j == 'Y':
+            score = score2 + points.get('Y') + 6
+        elif i == 'A' and j == 'Z':
+            score = score2 + points.get('Z')
+        elif i == 'B' and j == 'X':
+            score = score2 + points.get('X')
+        elif i == 'B' and j == 'Y':
+            score = score2 + points.get('Y') + 3
+        elif i == 'B' and j == 'Z':
+            score = score2 + points.get('Z') + 6
+        elif i == 'C' and j == 'X':
+            score = score2 + points.get('X') + 6
+        elif i == 'C' and j == 'Y':
+            score = score2 + points.get('Y')
+        elif i == 'C' and j == 'Z':
+            score = score2 + points.get('Z') + 3
 
 x = getInput()
 print(getInput())
 print(findScore(x))
+print(task2(x))
 
-#********************TEST********************
-'''def test_findScore():
-    y = [('A', 'Y'), ('B', 'X'), ('C', 'Z')]
-    assert findScore(y) == 15'''
