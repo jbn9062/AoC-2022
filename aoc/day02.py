01cc7fdd517ed63f9afcdf7a3547058be183fb43
jbn9062 <jbnjibin9062@gmail.com>
2022-12-05 20:44:04 +0100

    day2

--- aoc/day02.py
+++ aoc/day02.py
@@ -6,7 +6,7 @@ def getInput():
         i.remove(' ')
     return x
 
-def findScore(x):
+def task1(x):
     points = {'A': 1, 'B': 2, 'C': 3, 'X': 1, 'Y': 2, 'Z': 3}
     score = 0
     for i, j in x:
@@ -33,32 +33,33 @@ def findScore(x):
     return score
 
 def task2(x):
-    points = {'A': 1, 'B': 2, 'C': 3, 'X': 1, 'Y': 2, 'Z': 3}
+    points = {'A': 1, 'B': 2, 'C': 3, 'X': 3, 'Y': 0, 'Z': 3}
     score2 = 0
-    for i in range(len(x)):
 
     for i, j in x:
         if i == 'A' and j == 'X':
-            score = score2 + points.get('X') + 3
+            score2 = score2 + points.get('C') + 0
         elif i == 'A' and j == 'Y':
-            score = score2 + points.get('Y') + 6
+            score2 = score2 + points.get('A') + 3
         elif i == 'A' and j == 'Z':
-            score = score2 + points.get('Z')
+            score2 = score2 + points.get('B') + 6
         elif i == 'B' and j == 'X':
-            score = score2 + points.get('X')
+            score2 = score2 + points.get('A') + 0
         elif i == 'B' and j == 'Y':
-            score = score2 + points.get('Y') + 3
+            score2 = score2 + points.get('B') + 3
         elif i == 'B' and j == 'Z':
-            score = score2 + points.get('Z') + 6
+            score2 = score2 + points.get('C') + 6
         elif i == 'C' and j == 'X':
-            score = score2 + points.get('X') + 6
+            score2 = score2 + points.get('B') + 0
         elif i == 'C' and j == 'Y':
-            score = score2 + points.get('Y')
+            score2 = score2 + points.get('C') + 3
         elif i == 'C' and j == 'Z':
-            score = score2 + points.get('Z') + 3
+            score2 = score2 + points.get('A') + 6
+
+    return score2
 
 x = getInput()
 print(getInput())
-print(findScore(x))
-print(task2(x))
+print("Task1: ", task1(x))
+print("Task2: ", task2(x))
 
