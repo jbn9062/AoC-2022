a6e53904b309b9e7de5079fbbb623702f134a8d5
jbn9062 <jbnjibin9062@gmail.com>
2022-12-07 04:03:17 +0100

    day2

--- aoc/day03.py
+++ aoc/day03.py
@@ -1,20 +1,48 @@
 from collections import Counter
 import string
 with open("day03.txt", "r") as f:
-    bag = [list(i) for i in f.read().split('\n')]
-    print(bag)
-part1 =[]
+    bag = [i for i in f.read().strip().split('\n')]
+part1 = []
 part2 = []
 common = []
 for i in bag:
     part1 = i[:len(i)//2]
     part2 = i[len(i)//2:]
+    print(i, part1, part2)
     for i in part1:
         if i in part2:
             common.append(i)
 
+
 x = Counter(common)
 print("Common: ", common)
-print(x)
+print("X: ",x)
+
+lowerAlpa = [str(i) for i in string.ascii_lowercase]
+upperAlpha = [str(i) for i in string.ascii_uppercase]
+alphas = lowerAlpa+upperAlpha
+print("Alphas", alphas)
+
+priors = {}
+for count, value in enumerate(alphas, 1):
+    priors[value] = count
+
+newPrior={}
+for k, v in priors.items():
+    for a, b in x.items():
+        if k == a:
+            newPrior[k] = v
+
+total = 0
+for k,v in newPrior.items():
+    if k in x:
+        total = total + v
+print("priors", priors)
+print("newPrior: ", newPrior)
+print(total)
+
+
+
+
 
 
