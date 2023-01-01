b6a6eebf7e82495dc0edbf2cce2f78a20336e85f
jbn9062 <jbnjibin9062@gmail.com>
2023-01-01 04:12:42 +0100

    day10

--- aoc/day10.py
+++ aoc/day10.py
@@ -10,12 +10,14 @@ d_list = [{i[0]: int(i[1])} for i in data]
 print(f"{len(d_list)} : {d_list}\n")
 
 
-cycles = [20, 60, 100, 140, 180, 220] #  @ 20: x = 21 st = 420
+cycles = [20, 60, 100, 140, 180, 220]
 x = 1
 cycle = 0
 signalStrength = 0
 draw = ""
 screen = []
+
+
 for i in d_list:
     if 'noop' in i.keys():
         cycle += 1
